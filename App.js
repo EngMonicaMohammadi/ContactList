c9fd27774826211b476e8c9955333958e190d98b
Monica <74226136+EngMonicaMohammadi@users.noreply.github.com>
2021-02-09 15:37:18 +0430

    contact-listusing_database

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons"
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator} from "@react-navigation/stack";
+import * as SQLite from 'expo-sqlite';
 const Stack = createStackNavigator();
+const db = SQLite.openDatabase('contacts.db')
 
 import Contact from './screen/Contacts'
 import CreateContact from './screen/CreateContacts'
 import Profile from './screen/Profile'
 import SearchScreen from './screen/SearchScreen'
 export default function App() {
+  useEffect(()=> {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists contact(id integer primary key autoincrement, name text, phone text, email text);'
+      ,[], ()=> console.log('Table created successfully'));
+    })
+  })
   return (
     <View style={styles.container}>
       <NavigationContainer>
@@ -32,10 +40,10 @@ export default function App() {
     </View>    
   );
 }
-// <SearchScreen />
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f8f4f4'
   },
 });
+
