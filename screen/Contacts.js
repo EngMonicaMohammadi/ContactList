62eff6e129dbfee7add93dc7ed0aadd343d5e62c
Monica <74226136+EngMonicaMohammadi@users.noreply.github.com>
2021-02-15 11:18:40 +0430

    9-contact-listselectinsertdelete

--- screen/Contacts.js
+++ screen/Contacts.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
     View,
     Text,
@@ -8,29 +8,40 @@ import {
 } from 'react-native'
 
 import {Feather} from '@expo/vector-icons'
+import *as SQLite from 'expo-sqlite';
 import colors from '../utils/colors'
 import ContactListItem from '../components/ContactListItem'
-const contacts = [
-    { id: '1', name: 'Monica', phone: '040443455', email: 'monica@gmail.com'},
-    { id: '2', name: 'Bahar', phone: '3553265', email: 'bahar@gmail.com'},
-    { id: '3', name: 'AhmadSina', phone: '040353255', email: 'ah_sina@gmail.com'},
-    { id: '4', name: 'SharifAhmad', phone: '0794340000', email: 'rasekh@gmail.com'},
-    { id: '5', name: 'Fatemah', phone: '07943300', email: 'sama@gmail.com'},
-    { id: '6', name: 'Samira', phone: '078965000', email: 'samira@gmail.com'},
-    { id: '7', name: 'Reza', phone: '072434012', email: 'habibi@gmail.com'},
-    { id: '8', name: 'Atefa', phone: '0782943023', email: 'Atefa@gmail.com'},
-    { id: '9', name: 'Maryam', phone: '0745347600', email: 'maryam@gmail.com'},
-]
+const db =SQLite.openDatabase('contact.db');
+
 export default function Contacts({navigation}){
+    const [contacts, setContacts] = useState([]);
+    useEffect(()=>{
+        db.transaction((tx)=>{
+           tx.executeSql('select * from contact', [], (tx,{rows})=>{
+            var data = [];
+            for(var i = 0; i< rows.length; i++){
+                data.push(rows[i]);
+            }
+            setContacts(data);
+           })
+        })
+    })
+    const deleteContact = (id) => {
+        db.transaction(tx => {
+            tx.executeSql('delete from contact where id = ?', [id])
+        })
+    }
    return (
        <View>
-    <FlatList
-    data={contacts}
-    keyExtractor={(item)=>item.id}
-    renderItem={({item}) => {
-        return <ContactListItem name={item.name} phone={item.phone} onPress={()=> navigation.navigate('Profile',{item:item})} />
-    }}
-    />
+        {contacts.length > 0 ? <FlatList
+        data={contacts}
+        keyExtractor={(item)=>item.id}
+        renderItem={({item}) => {
+            return <ContactListItem name={item.name} phone={item.phone} onPress={()=> navigation.navigate('Profile',{item:item})} onDeleteContact={()=> deleteContact(item.id)} />
+        }}
+        />: <View>
+                <Text>No contact to display</Text>
+            </View>}
     <TouchableOpacity style={styles.floatButton} onPress={()=> navigation.navigate('CreateContact')}> 
         <Text>
         <Feather name="plus" size={28} color="white" />
